b1714302c51bb302376ac970187d95594f9aa8c4
clamar92 <cla.mar92@gmail.com>
2026-05-10 10:36:05 +0200

    first commit

---
+++ resources/thoughts.py
@@ -0,0 +1,51 @@
+from flask_restx import Namespace, Resource, fields, reqparse
+from flask import request
+from models import ThoughModel
+from extensions import db
+from datetime import datetime, UTC
+
+api = Namespace("thoughts", description="Gestione degli articoli")
+
+thought_model = api.model("Thought", {
+    "id": fields.Integer, "username": fields.String,
+    "text": fields.String, "timestamp": fields.DateTime
+})
+
+parser = reqparse.RequestParser()
+parser.add_argument("username", required=True, help="Username obbligatorio")
+parser.add_argument("text", required=True, help="Testo obbligatorio")
+parser.add_argument("Authorization", location="headers",required=True,
+                    help="Bearer valid-token")
+
+def validate_token(token):
+    return token == "Bearer valid-token"
+
+@api.route("/")
+class ThoughList(Resource):
+
+    @api.marshal_list_with(thought_model)
+    def get(self):
+        return ThoughModel.query.all()
+    
+    @api.expect(parser)
+    @api.marshal_with(thought_model)
+    def post(self):
+        token = request.headers.get("Authorization")
+        if not validate_token(token):
+            api.abort(401, "Token non valido")
+        args = parser.parse_args()
+        new_thought = ThoughModel(username=args["username"],
+                                  text=args["text"],
+                                  timestamp=datetime.now(UTC))
+        db.session.add(new_thought)
+        db.session.commit()
+        return new_thought, 201
+    
+@api.route("/<int:id>")
+class Thought(Resource):
+    @api.marshal_with(thought_model)
+    def get(self,id):
+        thought = db.session.get(ThoughModel, id)
+        if not thought:
+            api.abort(404, "Though non trovato")
+        return thought
